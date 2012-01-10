21507014aa061af90238b11ebb243d5ddc98be75
chandra <chandra@cyclops.(none)>
2012-01-10 07:00:38 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Techbot3::Application.config.secret_token = '9dacb47d7bca7516616cda945a6e3f9bae6d47d9b954d634c5d0b086a42e491f9bf93e4084a1ab6518101a28f59bd391ec86a7d5f72c06475931358518ed0e04'
