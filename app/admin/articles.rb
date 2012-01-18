0215092ad494794daf998a1435f72da8f7201f80
chandra <chandra@cyclops.(none)>
2012-01-18 17:48:06 +0530

    Change articles admin columns

--- app/admin/articles.rb
+++ app/admin/articles.rb
@@ -1,3 +1,14 @@
 ActiveAdmin.register Article do
-  
+  index do
+    column :id
+    column :title
+    column :abstract
+    column :publish_date
+    column :rank
+    column :reads
+    column :comments_attracted
+    column :created_at
+    column :updated_at
+    default_actions
+  end
 end
