6f283f157de9d951d8ec099ef65fb0a9f1239923
chandra <chandra@cyclops.(none)>
2012-01-19 08:10:44 +0530

    Added forgot password functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,5 +11,18 @@ class UsersController < ApplicationController
       render :new
     end
   end
-
+  
+  def reset_password
+    @user = User.first(:email => params[:email])
+    if @user
+      @user.deliver_reset_password_instructions!
+      redirect_to forgot_password, :notice => "A mail has been sent to #{params[:email]}"
+    else
+      redirect_to forgot_password, :alert => "No email #{params[:email]} found in our user database !!"
+    end
+  end
+  
+  def forgot_password
+    render :forgot_password
+  end 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,15 +4,22 @@ Techbot3::Application.routes.draw do
 
   devise_for :admin_users, ActiveAdmin::Devise.config
 
+  # User management
   resources :users, :except => [:index]
   resources :sessions, :only => [:create, :destroy]
+
   get "login" => "sessions#new", :as => :login
   match "logout" => "sessions#destroy", :as => :logout
   get "register" => "users#new", :as => :register
 
+  get "forgot_password" => "users#forgot_password", :as => :forgot_password
+  post "password_reset" => "users#reset_password", :as => :reset_password
+
+  # Root path management
   root :to => 'editions#latest'
-  
   get 'home' => 'editions#latest', :as => :home
+  
+  # Read edition
   get 'editions/:id' => 'editions#show', :as => :read_edition
   get 'articles/:id' => 'articles#show', :as => :read_article
   post 'articles/:id/comment' => 'comments#create', :as => :post_comment
