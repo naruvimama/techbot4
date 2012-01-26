1e3f97bba3374adeda3e5d527e35ecdc4266139d
chandra <chandra@cyclops.(none)>
2012-01-26 09:58:30 +0530

    Removing boot strap --- style next without bootstrap

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,9 +4,9 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = login(params[:email], params[:password], params[:remember_me])
+    user = login(params[:email], params[:password],params[:remember_me] )
     if user
-      redirect_back_or_to root_url, :notice => "Logged in!"
+      redirect_back_or_to root_url, :notice => "Welcome #{user.username}, You have been signed in!"
     else
       flash.now.alert = "Email or password was invalid."
       redirect_to :login
@@ -15,6 +15,6 @@ class SessionsController < ApplicationController
   
   def destroy
     logout
-    redirect_back_or_to root_url, :notice => "Logged in!"
+    redirect_back_or_to root_url, :notice => "You have been logged out !"
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,5 @@ class User < ActiveRecord::Base
   validates_confirmation_of :password
   validates_presence_of :password, :on => :create
   validates_presence_of :email
-  validates_uniqueness_of :email
+  validates_uniqueness_of :email, :username
 end
